db902f4e126e6288e78d454af1ad19860c0d6945
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-01-05 01:00:07 -0400

    Subida de archivos de configuracion y fuente del Blog

---
+++ mail/contact_me.php
@@ -0,0 +1,24 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No proporciono la informacion necesaria.";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = 'yourname@yourdomain.com'; // Add your email address inbetween the '' replacing yourname@yourdomain.com - This is where the form will send a message to.
+$email_subject = "Consulta GDG Android Bolivia Blog:  $name";
+$email_body = "Recibio un nuevo mensaje desde su pagina de contacto de su sitio web.\n\n"."Aqui tiene los detalles:\n\nNombre: $name\n\nCorreo electronico: $email_address\n\nMensaje:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.
+$headers .= "Reply-To: $email_address";   
+mail($to,$email_subject,$email_body,$headers);
+return true;         
+?>
